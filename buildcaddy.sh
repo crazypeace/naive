1376b4200ebbbb9652451037c06abfc46f52b0f0
crazypeace <665889+crazypeace@users.noreply.github.com>
2022-11-08 15:20:09 +0800

    Update buildcaddy.sh

--- buildcaddy.sh
+++ buildcaddy.sh
@@ -2,6 +2,8 @@
 # https://github.com/shell-script/naivecaddy/blob/master/naivecaddy.sh
 # https://lhy.life/20211218-naiveproxy/
 
+save_dir=$(pwd)
+
 GO_LATEST_VER=$(curl https://go.dev/VERSION?m=text)
 
 case "$(uname -m)" in
@@ -20,7 +22,7 @@ case "$(uname -m)" in
 esac
 
 # 安装 Go 编译环境
-mkdir -p /tmp/go && cd $_
+mkdir -p /tmp/go_naive_caddy && cd $_
 rm *
 wget "https://go.dev/dl/${GO_LATEST_VER}.linux-${SYSTEM_ARCH}.tar.gz"
 rm -rf /usr/local/go && tar -C /usr/local -xzf go*.tar.gz
@@ -31,3 +33,6 @@ go version
 go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
 ~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
 ./caddy version
+
+cd $(save_dir)
+cp /tmp/go_naive_caddy/caddy ./
